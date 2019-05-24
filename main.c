381e7b557728cc9ca1b7e9e546afe65bc79b059e
Kiana Mills <kmills@vo-d1.21-school.ru>
2019-05-24 22:27:28 +0300

    struct stat all printed rovno

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 13:47:18 by kmills            #+#    #+#             */
-/*   Updated: 2019/05/24 22:16:31 by kmills           ###   ########.fr       */
+/*   Updated: 2019/05/24 22:27:14 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,28 @@ int		main(int argc, char **argv)
 	struct timespec	ff;
 
 	sstat = stat("libft", &buf);
-	printf("st_atimespec.tv_nsec %ld\n", buf.st_atimespec.tv_nsec);
-	printf("st_atimespec.tv_sec %ld\n", buf.st_atimespec.tv_sec);
-	printf("st_birthtimespec.tv_nsec %ld\n", buf.st_birthtimespec.tv_nsec);
-	printf("st_birthtimespec.tv_sec %ld\n", buf.st_birthtimespec.tv_sec);
-	printf("st_blksize %u\n", buf.st_blksize);
-	printf("st_blocks %llu\n", buf.st_blocks);
-	printf("st_ctimespec.tv_nsec %ld\n", buf.st_ctimespec.tv_nsec);
-	printf("st_ctimespec.tv_sec %ld\n", buf.st_ctimespec.tv_sec);
-	printf("st_dev %u\n", buf.st_dev);
-	printf("st_flags %u\n", buf.st_flags);
-	printf("st_gen %u\n", buf.st_gen);
-	printf("st_gid %u\n", buf.st_gid);
-	printf("st_ino %llu\n", buf.st_ino);
-	printf("st_lspare %u\n", buf.st_lspare);
-	printf("st_mode %u\n", buf.st_mode);
-	printf("st_mtimespec.tv_nsec %ld\n", buf.st_mtimespec.tv_nsec);
-	printf("st_mtimespec.tv_sec %ld\n", buf.st_mtimespec.tv_sec);
-	printf("st_nlink %u\n", buf.st_nlink);
-	printf("st_qspare %lld\n", (*buf.st_qspare));
-	printf("st_rdev %u\n", buf.st_rdev);
-	printf("st_size %lld\n", buf.st_size);
-	printf("st_uid %u\n", buf.st_uid);
+	printf("st_atimespec.tv_nsec %30ld\n", buf.st_atimespec.tv_nsec);
+	printf("st_atimespec.tv_sec %31ld\n", buf.st_atimespec.tv_sec);
+	printf("st_birthtimespec.tv_nsec %26ld\n", buf.st_birthtimespec.tv_nsec);
+	printf("st_birthtimespec.tv_sec %27ld\n", buf.st_birthtimespec.tv_sec);
+	printf("st_blksize %40u\n", buf.st_blksize);
+	printf("st_blocks %41llu\n", buf.st_blocks);
+	printf("st_ctimespec.tv_nsec %30ld\n", buf.st_ctimespec.tv_nsec);
+	printf("st_ctimespec.tv_sec %31ld\n", buf.st_ctimespec.tv_sec);
+	printf("st_dev %44u\n", buf.st_dev);
+	printf("st_flags %42u\n", buf.st_flags);
+	printf("st_gen %44u\n", buf.st_gen);
+	printf("st_gid %44u\n", buf.st_gid);
+	printf("st_ino %44llu\n", buf.st_ino);
+	printf("st_lspare %41u\n", buf.st_lspare);
+	printf("st_mode %43u\n", buf.st_mode);
+	printf("st_mtimespec.tv_nsec %30ld\n", buf.st_mtimespec.tv_nsec);
+	printf("st_mtimespec.tv_sec %31ld\n", buf.st_mtimespec.tv_sec);
+	printf("st_nlink %42u\n", buf.st_nlink);
+	printf("st_qspare %41lld\n", (*buf.st_qspare));
+	printf("st_rdev %43u\n", buf.st_rdev);
+	printf("st_size %43lld\n", buf.st_size);
+	printf("st_uid %44u\n", buf.st_uid);
 	ft_strlen("124");
 	return (0);
 }
