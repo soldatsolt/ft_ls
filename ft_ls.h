1efd1ef90b229615f286160273756e7256a00419
Kiana Mills <kmills@vo-d3.21-school.ru>
2019-05-24 14:32:00 +0300

    all includes in ft_ls.h

--- ft_ls.h
+++ ft_ls.h
@@ -6,12 +6,21 @@
 /*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 13:46:27 by kmills            #+#    #+#             */
-/*   Updated: 2019/05/24 13:46:57 by kmills           ###   ########.fr       */
+/*   Updated: 2019/05/24 14:26:25 by kmills           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 # include "./libft/libft.h"
+# include <sys/stat.h>
+# include <sys/xattr.h>
+# include <time.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <pwd.h>
+# include <uuid/uuid.h>
+# include <grp.h>
+# include <uuid/uuid.h>
 
 #endif
